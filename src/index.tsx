103332531b5de8771c51e07ee32e8064cab13f26
ben.lynch <ben.lynch@leadventure.com>
2024-06-18 10:58:39 -0500

    understanding component state

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,46 @@ interface AppProps {
   color?: string;
 }
 
+/*
+interface AppState {
+  counter: number;
+} 
+*/
+
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };  
+
+  /*
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0  }
+  }
+  */
+ 
+  onIncrement = () : void => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement = () : void => {
+    this.setState({ counter: this.state.counter - 1 });
+  } 
+
+
   render() {
-    return <div>Hi there  {this.props.color}!</div>;
+    return (
+      <div>
+        <h1 style={{ color: this.props.color }}>
+          Hello, World! {this.state.counter}
+        </h1>
+        <button onClick={this.onIncrement}>
+          Increment
+        </button>
+        <button onClick={this.onDecrement}>
+          Decrement 
+        </button>
+      </div>
+    );
   }
 }
  
