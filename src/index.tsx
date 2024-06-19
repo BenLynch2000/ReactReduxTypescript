a9776a401b9e088aa0da96c3c8fc920c2c0fd015
ben.lynch <ben.lynch@leadventure.com>
2024-06-19 10:52:23 -0500

    initial skeleton of test project

--- src/index.tsx
+++ src/index.tsx
@@ -1,85 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
+
 const el = document.getElementById("root");
- 
 const root = ReactDOM.createRoot(el!);
- 
-interface AppProps {
-  color?: string;
-}
 
-/*
-interface AppState {
-  counter: number;
-} 
-*/
+const store = createStore(reducers, applyMiddleware(thunk));
 
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };  
-
-  /*
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0  }
-  }
-  */
- 
-  onIncrement = () : void => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
-  onDecrement = () : void => {
-    this.setState({ counter: this.state.counter - 1 });
-  } 
-
-
-  render() {
-    return (
-      <div>
-        <h1 style={{ color: this.props.color }}>
-          Hello, World! {this.state.counter}
-        </h1>
-        <button onClick={this.onIncrement}>
-          Increment
-        </button>
-        <button onClick={this.onDecrement}>
-          Decrement 
-        </button>
-      </div>
-    );
-  }
-}
-
-root.render(<App />);
-
-/*
-
-const App = (props: AppProps) : JSX.Element => {
-  const [counter, setCounter] = React.useState(0);
-
-  const onIncrement = () => {
-    setCounter(counter + 1);
-  }
-
-  const onDecrement = () => {
-    setCounter(counter - 1);
-  }
-
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>
-        Hello, World! {counter}
-      </h1>
-      <button onClick={onIncrement}>
-        Increment
-      </button>
-      <button onClick={onDecrement}>
-        Decrement 
-      </button>
-    </div>
-  );
-} 
-  */
